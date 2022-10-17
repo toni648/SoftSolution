cad72c1b899eb2a96374700ac91a3b4d0cad3f7c
SunDoLa <115986587+SunDoLa@users.noreply.github.com>
2022-10-17 13:15:56 +0200

    Add files via upload

---
+++ SoftSolution/src/main/java/Ablesebogen.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Ablesebogen {
+    String kundenNummer;
+    int zählernummer;
+    Date datum = new Date();
+    boolean neu_eingebaut;
+    int zählerstand;
+    String kommentar;
+}
