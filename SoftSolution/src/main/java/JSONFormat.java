1addd471099859e2067049e16b997038445aa692
toni <brnadicantonio@gmail.com>
2022-10-19 13:58:13 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	SoftSolution/src/main/java/GUI.java

---
+++ SoftSolution/src/main/java/JSONFormat.java
@@ -0,0 +1,13 @@
+import org.json.JSONObject;
+
+public class JSONFormat {
+    JSONObject jason = new JSONObject();
+
+    public JSONObject getJason() {
+        return jason;
+    }
+
+    public void setJason(JSONObject jason) {
+        this.jason = jason;
+    }
+}
