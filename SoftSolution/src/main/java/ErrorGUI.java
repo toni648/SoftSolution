505aa5506e1be8066c5cdea2c81ce9b028c11973
SunDoLa <sun12dola@t-online.de>
2022-10-20 08:29:43 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ SoftSolution/src/main/java/ErrorGUI.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ErrorGUI extends JFrame {
+    public ErrorGUI(String inhalt){
+
+        super("Errormeldung");
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                exit();
+            }
+        });
+
+        final Container con = getContentPane();
+        con.setLayout(new BorderLayout());
+        final JLabel meldung = new JLabel(inhalt);
+        final JButton okButton = new JButton("OK");
+
+        con.add(meldung, BorderLayout.NORTH);
+        con.add(okButton, BorderLayout.CENTER);
+
+        okButton.addActionListener(new ActionListener() {
+
+
+        }
+    }
+}
